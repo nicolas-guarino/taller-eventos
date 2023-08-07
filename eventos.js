e9c95f9671a7c1b32e1aba81fa6217f9d63b4658
Nicolas-Guarino <nicolasguarino03@gmail.com>
2023-08-07 16:05:47 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,5 +1,9 @@
 const div = document.getElementById('contenedor');
 
-div.addEventListener('click', function() {
-    alert('Hola! Soy el div')
+div.addEventListener('click', function(event) {
+    if (event.target !== document.getElementById('contenedor')) {
+        return
+    } else {
+        return alert('Hola! Soy el div')
+    }
 });
